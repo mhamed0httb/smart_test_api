628646c3644b74ced6b7314fd0d9543a5c998a72
Mhamed Hattab <mhamed.httb@gmail.com>
2017-01-31 05:45:47 +0100

    fix things

--- getRandomQuestion.php
+++ getRandomQuestion.php
@@ -4,22 +4,29 @@
     $sql=mysqli_query($dbc,"select * from question where chosen=false");
 	$result = array();
 	$questionsId = array();
-	while($res=mysqli_fetch_array($sql)){
-		//array_push($result, $res);
-		array_push($questionsId, $res['id']);
+	
+	if(mysqli_num_rows($sql)==0){
+		echo 'nothing';
+		}else{
+		
+		while($res=mysqli_fetch_array($sql)){			
+			array_push($questionsId, $res['id']);
+		}
+		
+		
+		$randomId = $questionsId[array_rand($questionsId)];
+		$sql2=mysqli_query($dbc,"select * from question where id=$randomId");
+		while($res=mysqli_fetch_array($sql2)){
+			array_push($result, $res);
+		}
+		echo json_encode($result);
+		
+		$sql3=mysqli_query($dbc,"update question set displayed_now=false");
+		$sql4=mysqli_query($dbc,"update question set displayed_now=true where id=$randomId");
+		$sql5=mysqli_query($dbc,"update question set chosen=true where id=$randomId");
+		
 	}
 	
-		
-	$randomId = $questionsId[array_rand($questionsId)];
-	$sql2=mysqli_query($dbc,"select * from question where id=$randomId");
-	while($res=mysqli_fetch_array($sql2)){
-		array_push($result, $res);
-	}
-	echo json_encode($result);
-
-	$sql3=mysqli_query($dbc,"update question set displayed_now=false");
-	$sql4=mysqli_query($dbc,"update question set displayed_now=true where id=$randomId");
-	$sql5=mysqli_query($dbc,"update question set chosen=true where id=$randomId");
 	
 	
 	
